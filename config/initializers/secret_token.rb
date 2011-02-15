a2706dcaf155833396fad0abd333f5a5d8719712
Josh <josh@codingforrent.com>
2011-02-15 21:01:30 +0800

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WdDeploy::Application.config.secret_token = '1b5696e427205721c99d03551f4bafde7f293112c72fa8faf7a522b5b91c53eee06890453dd681092c17ef2a339625f26dda83e76ee6903902b61d905300ca84'
